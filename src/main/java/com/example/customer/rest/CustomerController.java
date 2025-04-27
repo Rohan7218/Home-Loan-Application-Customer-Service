71f08b7e0922f625cabc5b85b804aaf3811f4003
Aman Pande <amanpande912@gmail.com>
2025-04-27 12:21:12 +0530

    getCustomerById , getAllCustomer and exception handling class implemented

--- src/main/java/com/example/customer/rest/CustomerController.java
+++ src/main/java/com/example/customer/rest/CustomerController.java
@@ -1,10 +1,13 @@
 package com.example.customer.rest;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.customer.dto.AdditionalCustomerDetailsDTO;
 import com.example.customer.dto.CustomerDocumentDTO;
+import com.example.customer.dto.getCustomerDetailsDTO;
 import com.example.customer.entity.CustomerDetails;
 import com.example.customer.service.CustomerService;
 
@@ -29,24 +33,41 @@ public class CustomerController {
 		return new ResponseEntity<String>(msg, HttpStatus.CREATED);
 	}
 
-
 	@PostMapping(value = "/documents/{personalDocumentId}")
-	public ResponseEntity<String> uploadDocuments(@ModelAttribute CustomerDocumentDTO customerDocumentDTO, @PathVariable Integer personalDocumentId)
-	{
-		String msg=customerService.uploadDocuments(customerDocumentDTO, personalDocumentId);
+	public ResponseEntity<String> uploadDocuments(@ModelAttribute CustomerDocumentDTO customerDocumentDTO,
+			@PathVariable Integer personalDocumentId) {
+		String msg = customerService.uploadDocuments(customerDocumentDTO, personalDocumentId);
 		return new ResponseEntity<String>(msg, HttpStatus.CREATED);
 	}
 
-
-	
-	
 	@PostMapping(value = "/addAdditionalDetails/{customerId}")
-	public ResponseEntity<String> addAdditionalCustomerDetails(@RequestBody @Valid AdditionalCustomerDetailsDTO additionalCustomerDetailsDTO, @PathVariable Integer customerId){
-		
-		String msg = customerService. addAdditionalCustomerDetails(additionalCustomerDetailsDTO , customerId);
+	public ResponseEntity<String> addAdditionalCustomerDetails(
+			@RequestBody @Valid AdditionalCustomerDetailsDTO additionalCustomerDetailsDTO,
+			@PathVariable Integer customerId) {
+
+		String msg = customerService.addAdditionalCustomerDetails(additionalCustomerDetailsDTO, customerId);
 		return new ResponseEntity<String>(msg, HttpStatus.CREATED);
-		
+
 	}
-	
+
+	@GetMapping(value = "/getCustomer/{customerId}")
+	public ResponseEntity<getCustomerDetailsDTO> getCustomerDetails(@PathVariable Integer customerId) {
+
+		getCustomerDetailsDTO getCustomer = customerService.getCustomerDetails(customerId);
+		return new ResponseEntity<getCustomerDetailsDTO>(getCustomer, HttpStatus.OK);
+
+	}
+
+	@GetMapping(value = "/getAllCustomers")
+	public ResponseEntity<List<CustomerDetails>> getAllCustomerDetails() {
+
+		List<CustomerDetails> getAllCustomers = customerService.getAllCustomerDetails();
+
+		if (!getAllCustomers.isEmpty()) {
+			return new ResponseEntity<List<CustomerDetails>>(getAllCustomers, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<List<CustomerDetails>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 }
--- src/main/java/com/example/customer/service/CustomerService.java
+++ src/main/java/com/example/customer/service/CustomerService.java
@@ -1,22 +1,24 @@
 package com.example.customer.service;
 
 
-import com.example.customer.dto.CustomerDocumentDTO;
+import java.util.List;
 
 import com.example.customer.dto.AdditionalCustomerDetailsDTO;
-
+import com.example.customer.dto.CustomerDocumentDTO;
+import com.example.customer.dto.getCustomerDetailsDTO;
 import com.example.customer.entity.CustomerDetails;
 
 public interface CustomerService {
 	String addCustomer(CustomerDetails customerDetails);
 
-
 	String uploadDocuments(CustomerDocumentDTO customerDocumentDTO, Integer personalDocumentId);
 
-	
-
-	
 	String addAdditionalCustomerDetails(AdditionalCustomerDetailsDTO additionalCustomerDetailsDTO , Integer customerId);
+
+
+	getCustomerDetailsDTO getCustomerDetails(Integer customerId);
+
+	List<CustomerDetails> getAllCustomerDetails();
 
 
 }
