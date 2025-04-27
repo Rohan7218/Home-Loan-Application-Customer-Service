531de79d92868b0d166d4d2571bbf0f60e02f6c9
Rohan7218 <sontakkerohan751@gmail.com>
2025-04-27 11:32:24 +0530

    customerStatus functionality added

--- src/main/java/com/example/customer/serviceimpl/CustomerServiceImpl.java
+++ src/main/java/com/example/customer/serviceimpl/CustomerServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.customer.dto.AdditionalCustomerDetailsDTO;
 import com.example.customer.dto.CustomerDocumentDTO;
+import com.example.customer.dto.CustomerStatusEnum;
 import com.example.customer.entity.AllPersonalDocs;
 import com.example.customer.entity.BankAccountDetails;
 import com.example.customer.entity.CibilDetails;
@@ -44,7 +45,9 @@ public class CustomerServiceImpl implements CustomerService
 		customerDetails.setCibilId(cibilDetails);
 
 		CustomerDetails customerDetailsSaved = customerRepository.save(customerDetails);
-		cibilDetails.setCustomerId(customerDetailsSaved.getCustomerId());
+								  customerDetailsSaved.setCustomerStatus(CustomerStatusEnum.INPROCESS);
+								  cibilDetails.setCustomerId(customerDetailsSaved.getCustomerId());
+								  
 		customerRepository.save(customerDetailsSaved);
 
 		return "!!!!....Customer Saved SuccessFully....!!!!";
