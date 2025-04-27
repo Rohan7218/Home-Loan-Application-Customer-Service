e6e79000ab89ac090797b7382d435549018efd00
Aashish71-stack <aashishghule52@gmail.com>
2025-04-27 11:36:01 +0530

    Update Customer details functionality added

--- src/main/java/com/example/customer/service/CustomerService.java
+++ src/main/java/com/example/customer/service/CustomerService.java
@@ -2,7 +2,7 @@ package com.example.customer.service;
 
 
 import com.example.customer.dto.CustomerDocumentDTO;
-
+import com.example.customer.dto.UpdateCustomerDetailsDTO;
 import com.example.customer.dto.AdditionalCustomerDetailsDTO;
 
 import com.example.customer.entity.CustomerDetails;
@@ -12,11 +12,13 @@ public interface CustomerService {
 
 
 	String uploadDocuments(CustomerDocumentDTO customerDocumentDTO, Integer personalDocumentId);
-
-	
-
 	
 	String addAdditionalCustomerDetails(AdditionalCustomerDetailsDTO additionalCustomerDetailsDTO , Integer customerId);
+
+
+	String updateCustomerDetails(UpdateCustomerDetailsDTO updateCustomerDetailsDTO, Integer customerId);
+
+	
 
 
 }
